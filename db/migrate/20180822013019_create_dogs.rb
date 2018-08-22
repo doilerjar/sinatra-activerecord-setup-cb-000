de89809a8ea886412c40e3e490be058f89b8b227
Kevin K <doilerjar@outlook.com>
2018-08-22 01:31:15 +0000

    automatically backed up by learn

--- db/migrate/20180822013019_create_dogs.rb
+++ db/migrate/20180822013019_create_dogs.rb
@@ -1,4 +1,12 @@
 class CreateDogs < ActiveRecord::Migration
-  def change
+  def up 
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+    
+  def down
+    drop_table :dogs
   end
+  
 end
